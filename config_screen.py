d4216c7a39d696d9fa4bb16fa3ae3f79df3f6fc5
Gabriel Merlin <gabrielcmerlin@gmail.com>
2024-11-18 15:52:58 -0300

    EU QUERO NESCAU BALL

---
+++ config_screen.py
@@ -0,0 +1,25 @@
+from glfw.GLFW import *
+
+WIDTH = 1920
+HEIGHT = 1080
+
+def init_window():
+    # glfw: initialize and configure
+    # ------------------------------
+    glfwInit()
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3)
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
+
+    # glfw window creation
+    # --------------------
+    window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", None, None)
+    if (window == None):
+
+        print("Failed to create GLFW window")
+        glfwTerminate()
+        return -1
+
+    glfwMakeContextCurrent(window)
+
+    return window
---
+++ keyboard.py
@@ -0,0 +1,68 @@
+from glfw import _GLFWwindow as GLFWwindow
+from glfw.GLFW import *
+from OpenGL.GL import *
+from camera import Camera, Camera_Movement
+import glm
+
+WIDTH = 1920
+HEIGHT = 1080
+
+camera = Camera(glm.vec3(0.0, 0.0, 3.0))
+lastX = WIDTH / 2.0
+lastY = HEIGHT / 2.0
+firstMouse = True
+
+# timing
+lastFrame = 0.0
+
+# glfw: whenever the mouse moves, this callback is called
+# -------------------------------------------------------
+def mouse_callback(window: GLFWwindow, xpos: float, ypos: float) -> None:
+    global lastX, lastY, firstMouse
+
+    if (firstMouse):
+
+        lastX = xpos
+        lastY = ypos
+        firstMouse = False
+
+    xoffset = xpos - lastX
+    yoffset = lastY - ypos # reversed since y-coordinates go from bottom to top
+
+    lastX = xpos
+    lastY = ypos
+
+    camera.ProcessMouseMovement(xoffset, yoffset)
+
+# glfw: whenever the mouse scroll wheel scrolls, this callback is called
+# ----------------------------------------------------------------------
+def scroll_callback(window: GLFWwindow, xoffset: float, yoffset: float) -> None:
+
+    camera.ProcessMouseScroll(yoffset)
+
+
+# process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+# ---------------------------------------------------------------------------------------------------------
+def processInput(window: GLFWwindow, deltaTime) -> None:
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS):
+        glfwSetWindowShouldClose(window, True)
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS):
+        camera.ProcessKeyboard(Camera_Movement.FORWARD, deltaTime)
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS):
+        camera.ProcessKeyboard(Camera_Movement.BACKWARD, deltaTime)
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS):
+        camera.ProcessKeyboard(Camera_Movement.LEFT, deltaTime)
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS):
+        camera.ProcessKeyboard(Camera_Movement.RIGHT, deltaTime)
+
+    return camera
+
+# glfw: whenever the window size changed (by OS or user resize) this callback function executes
+# ---------------------------------------------------------------------------------------------
+def framebuffer_size_callback(window: GLFWwindow, width: int, height: int) -> None:
+
+    # make sure the viewport matches the new window dimensions note that width and 
+    # height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height)
