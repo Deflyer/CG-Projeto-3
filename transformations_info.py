249e16a7dc778adcd54b7bcb6cc188a19c216d23
Gabriel Merlin <gabrielcmerlin@gmail.com>
2024-11-25 16:46:47 -0300

    Documented transformations_info

--- transformations_info.py
+++ transformations_info.py
@@ -1,9 +1,15 @@
+# Aggregates all the information about the trasformations (translation, scale, rotation) of our objects.
+
 import glm
-import keyboard as kb
 import random
 import math
+import keyboard as kb
 
 def get_positions():
+    '''
+    Returns the position of all objects.
+    '''
+
     return [
         glm.vec3(-2.80, -0.80, -6.80),  # rose
         glm.vec3(kb.bird_radius * math.cos(kb.bird_angle), 50.0, kb.bird_radius * math.sin(kb.bird_angle)), # bird
@@ -17,84 +23,111 @@ def get_positions():
         glm.vec3(-1.0, -2.0, -1.0),     # plant2
         glm.vec3(-1.0, -2.0, -1.0),     # plant1
         glm.vec3(2.70, 1.55, -7.0),     # lamp
-        glm.vec3(kb.shrek_side_step + 1.60, -0.5, 2.0 + kb.shrek_step),        # lantern
+        glm.vec3(kb.shrek_side_step + 1.60, -0.5, 2.0 + kb.shrek_step), # lantern
         glm.vec3(2.70, 0.7, -20.0),     # magic_ball
         glm.vec3(2.70, 0.7, -20.0),     # magic_ball_stand
-        glm.vec3(kb.shrek_side_step, -1.0, 2.0 + kb.shrek_step)        # shrek
+        glm.vec3(kb.shrek_side_step, -1.0, 2.0 + kb.shrek_step)         # shrek
     ]
 
-# Function to compute scales dynamically
 def get_scales():
+    '''
+    Returns the scales of all objects. Used to compute scales dynamically.
+    '''
+
     return [
-        glm.vec3(0.05, kb.rose_scale_y, 0.05),  # rose
+        glm.vec3(0.05, kb.rose_scale_y, 0.05), # rose
         0.05,  # bird
         0.75,  # drawer
-        0.035,  # bed
-        0.003,  # bathroom
-        70.0,   # ground
-        0.5,    # house
-        1.0,    # sky
-        0.005,  # vase
-        0.05,   # plant2
-        0.05,   # plant1
-        0.03,   # lamp
-        0.01, # lantern
-        0.03,   # magic_ball
-        0.03,   # magic_ball_stand
-        2.8     # shrek
+        0.035, # bed
+        0.003, # bathroom
+        70.0,  # ground
+        0.5,   # house
+        1.0,   # sky
+        0.005, # vase
+        0.05,  # plant2
+        0.05,  # plant1
+        0.03,  # lamp
+        0.01,  # lantern
+        0.03,  # magic_ball
+        0.03,  # magic_ball_stand
+        2.8    # shrek
     ]
 
 def get_rotations():
+    '''
+    Returns the rotation angles of all objects.
+    '''
+
     return [
-        0.0,  # rose
-        - ( (360 * kb.bird_angle) / (2 * math.pi) ),  # bird
-        180.0,  # drawer
-        0.0,  # bed
-        200.0,  # bathroom
-        0.0,  # ground
-        180.0,  # house
-        kb.sky_rotation_angle,  # sky
-        0.0,  # vase
+        0.0,   # rose
+        - ( (360 * kb.bird_angle) / (2 * math.pi) ), # bird
+        180.0, # drawer
+        0.0,   # bed
+        200.0, # bathroom
+        0.0,   # ground
+        180.0, # house
+        kb.sky_rotation_angle, # sky
+        0.0,   # vase
         0.05,  # plant2
         0.05,  # plant1
         0.0,   #lamp
-        0.0, # lantern
+        0.0,   # lantern
         0.0,   # magic ball
         0.0,   # magic_ball_stand
         180.0  # shrek
     ]
 
 def get_position(idx):
+    '''
+    Returns the position of a given object, identified by its list index.
+    '''
+
     return get_positions()[idx]
 
 def get_scale(idx):
-    # Dynamically fetch scales
+    '''
+    Returns the scale of a given object, identified by its list index.
+    '''
+
     return get_scales()[idx]
 
 def get_rotation(idx):
+    '''
+    Returns the rotation angle of a given object, identified by its list index.
+    '''
+
     return get_rotations()[idx]
 
 def get_lights_positions():
+    '''
+    Returns the position of all lights.
+    '''
+
     obj_pos = get_positions()
-    # Positions of the point lights
+
+    # Positions of the point lights.
     pointLightPositions = [
         glm.vec3(10, 20.55, -7.0),
         glm.vec3(2.70, 0.7, -20.0),
-        obj_pos[12], # lantern position
+        obj_pos[12], # Lantern position.
         glm.vec3(0.0, 0.0, -3.0)
     ]
 
     return pointLightPositions
 
 def get_positions_plants():
-    positions1 = []
-  
+    '''
+    Returns the positions of our plants. We randomize its places to get a more dynamic scene.
+    '''
+
+    # House and bathroom (plants cant grow in these areas).
     exclusion_zones = [
     (-8, -30, 7, 60),
     (5, -9, 26, -2),             
     ]
     
-    # Calcula posições fixas para as plantas
+    # Calculates fixed positions for the plants.
+    positions1 = []
     for i in range(3):
         min_radius = 14 * (i + 1)
         max_radius = 14 * (i + 2)
@@ -118,9 +151,8 @@ def get_positions_plants():
             if valid:
                 positions1.append((t_x, t_y, t_z, angle, s_x, s_y, s_z))
 
+    # Calculates fixed positions for the plants.
     positions2 = []
-    
-    # Calcula posições fixas para as plantas
     for i in range(3):
         min_radius = 3 * (i + 1)
         max_radius = 3 * (i + 2)
@@ -143,4 +175,5 @@ def get_positions_plants():
                     valid = False
             if valid:
                 positions2.append((t_x, t_y, t_z, angle, s_x, s_y, s_z))
+
     return [positions1, positions2]
